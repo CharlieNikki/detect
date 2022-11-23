ab3c719616376008e74e6ed304303acf222a72f8
Charlie <charlienikki999@gmail.com>
2022-11-23 14:47:50 +0800

    增加了从数据库读取图片返回前端的功能

--- src/test/java/com/example/detect/DemoApplicationTests.java
+++ src/test/java/com/example/detect/DemoApplicationTests.java
@@ -1,15 +1,11 @@
 package com.example.detect;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
+
 class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-
-
-    }
 
 }
